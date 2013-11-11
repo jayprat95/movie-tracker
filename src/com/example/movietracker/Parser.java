26c9bfac646168102c989cc580afe721f48fcd4b
oliverek <odek@vt.edu>
2013-11-10 19:14:32 -0500

    Added drive.json file for testing. Also started changes to parser class

--- src/com/example/movietracker/Parser.java
+++ src/com/example/movietracker/Parser.java
@@ -1,6 +1,34 @@
 package com.example.movietracker;
 
+import java.lang.Object;
+import java.lang.String;
+import org.json.*;
+
 public class Parser
 {
-    //parse
+
+    //~Fields--------------------------------------------------------
+    private String stringToParse;
+    private int size;
+
+    //~Methods-------------------------------------------------------
+
+    /**
+     * This is the constructor for the parser that extracts data
+     * from the imdb database.
+     */
+    public Parser(String inputString) {
+
+        stringToParse = inputString;
+    }
+
+    public JSONObject createJSONObject() {
+        JSONObject(this.getStringToParse());
+
+    }
+
+
+    public String getStringToParse() {
+        return stringToParse;
+    }
 }
