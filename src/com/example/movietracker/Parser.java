d3b2c3935e9993b17feaf6acf3e2d87f72949f84
oliverek <odek@vt.edu>
2013-11-10 19:33:15 -0500

    Added json-simple jar

--- src/com/example/movietracker/Parser.java
+++ src/com/example/movietracker/Parser.java
@@ -3,13 +3,14 @@ package com.example.movietracker;
 import java.lang.Object;
 import java.lang.String;
 import org.json.*;
+import org.json.simple.parser.JSONParser;
+import java.io.FileReader;
 
 public class Parser
 {
 
     //~Fields--------------------------------------------------------
     private String stringToParse;
-    private int size;
 
     //~Methods-------------------------------------------------------
 
@@ -22,11 +23,16 @@ public class Parser
         stringToParse = inputString;
     }
 
-    public JSONObject createJSONObject() {
-        JSONObject(this.getStringToParse());
-
+    public JSONObject createJSONObjectFromJSONArray(JSONArray array) {
+        // empty
+        return new JSONObject();
     }
 
+    private JSONArray pullJSONDataToJSONArray(String fileLocation) {
+        JSONParser parser = new JSONParser();
+        JSONArray array = (JSONArray) parser.parse(new FileReader("drive.json"));
+
+    }
 
     public String getStringToParse() {
         return stringToParse;
