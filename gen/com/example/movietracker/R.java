131102245a93a5baa2d725c080a8c039c73b50ed
oliverek <odek@vt.edu>
2013-11-26 13:51:00 -0500

    Sorry, last was broken with tried tabs on gui.

--- gen/com/example/movietracker/R.java
+++ gen/com/example/movietracker/R.java
@@ -24,10 +24,7 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080003;
-        public static final int tab1=0x7f080000;
-        public static final int tab2=0x7f080001;
-        public static final int tab3=0x7f080002;
+        public static final int action_settings=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main_movie_view=0x7f030000;
