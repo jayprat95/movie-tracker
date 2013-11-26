02db2a28a70cf7eb17beb3b4f8af3208ee241afc
oliverek <odek@vt.edu>
2013-11-26 14:35:58 -0500

    Crap. messed up layout again

--- src/com/example/movietracker/DrawerLayout.java
+++ src/com/example/movietracker/DrawerLayout.java
@@ -1,21 +1,11 @@
 package com.example.movietracker;
 
-import android.os.Bundle;
 import android.app.Activity;
 
-public class DrawerLayout extends Activity
+public class DrawerLayout extends MainMovieView
 {
 
     public DrawerLayout() {
         super();
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_movie_view);
-
-
     }
 }
