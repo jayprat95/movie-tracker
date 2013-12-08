e0364eb7933cc6239758f8a64e469b9983934402
Jayanth Prathipati <jayanth@vt.edu>
2013-12-08 17:52:22 -0500

    commenting of MainMovie is complete

--- src/com/example/movietracker/MainMovieActivityTest.java
+++ src/com/example/movietracker/MainMovieActivityTest.java
@@ -13,8 +13,8 @@ import junit.framework.TestCase;
 // -------------------------------------------------------------------------
 /**
  * This class tests the Main Movie Activity in order to ensure that it is
- * working properly. This class extends the ActivityInstrumentationTestCase2
- * so that we can call individual elements in the Activity, such as any of the
+ * working properly. This class extends the ActivityInstrumentationTestCase2 so
+ * that we can call individual elements in the Activity, such as any of the
  * buttons and test them as we please.
  *
  * @author jayanth
@@ -41,7 +41,9 @@ public class MainMovieActivityTest
     // ----------------------------------------------------------
     /**
      * Create a new MainMovieActivityTest object.
-     * @param activityClass is the mainMovie activity
+     *
+     * @param activityClass
+     *            is the mainMovie activity
      */
     public MainMovieActivityTest(Class<MainMovieActivity> activityClass)
     {
@@ -49,53 +51,69 @@ public class MainMovieActivityTest
     }
 
     // ~Fields................................................................
+    // creates a Main Movie Activity
     private MainMovieActivity mma;
+    // creates an intent that we can use to test the buttons with
     private Intent            intent;
+    // makes an instrumentation class so that we can get data for assertions
     private Instrumentation   mInstrumentation;
 
-    //Widgets
-    Button toWatchButton;
-    Button watchedButton;
-    Button favoriteButton;
-    Button searchButton;
+    // Widgets
+    // these are the 4 buttons that are on the main movie activity
+    private Button            toWatchButton;
+    private Button            watchedButton;
+    private Button            favoriteButton;
+    private Button            searchButton;
 
 
+    /**
+     * Sets up the test class
+     */
     protected void setUp()
         throws Exception
     {
         super.setUp();
+        // gets the instrumentation from the test class
         this.mInstrumentation = getInstrumentation();
-        //intent = new Intent();
-        //setActivityIntent(intent);
-        //mma = this.getActivity();
     }
 
 
     /**
-     * Test method for onCreate
+     * Test method for onCreate, tests what happens when the activity is first
+     * initialized
      */
     public void testOnCreateBundle()
     {
+        // gets the activity
         mma = this.getActivity();
+        // gets the textfield that is created and hidden on the main view
         EditText text = (EditText)mma.findViewById(R.id.searchText);
+        // asserts that the EditText's visibility is being properly set
         assertEquals(View.GONE, text.getVisibility());
     }
 
 
     /**
-     * Test method for setupListeners
+     * Test method for SetUpListeners. This test method covers the case where
+     * the toWatch button is clicked
      */
     public void testOnClickToWatch()
     {
-    	mma = this.getActivity();
-        toWatchButton =
-            (Button)mma.findViewById(R.id.toWatch);
+        // gets the activity
+        mma = this.getActivity();
+        // gets the button on the activity
+        toWatchButton = (Button)mma.findViewById(R.id.toWatch);
+        /**
+         * gets the activity monitor so we can monitor what changes when the
+         * button is clicked
+         */
         ActivityMonitor activityMonitor =
             getInstrumentation().addMonitor(
                 ListMovieActivity.class.getName(),
                 null,
                 false);
         System.out.println(toWatchButton);
+        // this try-catch block clicks the button necessary as per the test
         try
         {
             runTestOnUiThread(new Runnable() {
@@ -111,24 +129,44 @@ public class MainMovieActivityTest
             e.printStackTrace();
         }
 
+        // gets the next activity, which would be the list of movies in this
+        // case
         Activity nextActivity =
             getInstrumentation().waitForMonitorWithTimeout(
                 activityMonitor,
                 30000);
+        // asserts that that list exists
         assertNotNull(nextActivity);
+        // finishes that activity
         nextActivity.finish();
     }
+
+
+    // ----------------------------------------------------------
+    /**
+     * Test method for SetUpListeners. This test method covers the case where
+     * the watched button is clicked
+     */
     public void testOnClickWatched()
     {
+        // gets the activity
+
         mma = this.getActivity();
-        watchedButton =
-            (Button)mma.findViewById(R.id.watched);
+        // gets the button on the activity
+        watchedButton = (Button)mma.findViewById(R.id.watched);
+        /**
+         * gets the activity monitor so we can monitor what changes when the
+         * button is clicked
+         */
         ActivityMonitor activityMonitor =
             getInstrumentation().addMonitor(
                 ListMovieActivity.class.getName(),
                 null,
                 false);
+
         System.out.println(watchedButton);
+        // this try-catch block clicks the button necessary as per the test
+
         try
         {
             runTestOnUiThread(new Runnable() {
@@ -143,25 +181,41 @@ public class MainMovieActivityTest
             System.out.println("Couldnt click button");
             e.printStackTrace();
         }
-
+        // gets the next activity, which would be the list of movies in this
+        // case
         Activity nextActivity =
             getInstrumentation().waitForMonitorWithTimeout(
                 activityMonitor,
                 30000);
+        // asserts that that list exists
         assertNotNull(nextActivity);
+        // finishes that activity
         nextActivity.finish();
     }
+
+
+    // ----------------------------------------------------------
+    /**
+     * Test method for SetUpListeners. This test method covers the case where
+     * the favorite button is clicked
+     */
     public void testOnClickFavorite()
     {
+        // gets the activity
         mma = this.getActivity();
-        favoriteButton =
-            (Button)mma.findViewById(R.id.favorite);
+        // gets the button on the activity
+        favoriteButton = (Button)mma.findViewById(R.id.favorite);
+        /**
+         * gets the activity monitor so we can monitor what changes when the
+         * button is clicked
+         */
         ActivityMonitor activityMonitor =
             getInstrumentation().addMonitor(
                 ListMovieActivity.class.getName(),
                 null,
                 false);
         System.out.println(favoriteButton);
+        // this try-catch block clicks the button necessary as per the test
         try
         {
             runTestOnUiThread(new Runnable() {
@@ -176,26 +230,41 @@ public class MainMovieActivityTest
             System.out.println("Couldnt click button");
             e.printStackTrace();
         }
-
+        // gets the next activity, which would be the list of movies in this
+        // case
         Activity nextActivity =
             getInstrumentation().waitForMonitorWithTimeout(
                 activityMonitor,
                 30000);
+        // asserts that that list exists
         assertNotNull(nextActivity);
+        // finishes that activity
         nextActivity.finish();
     }
 
+
+    // ----------------------------------------------------------
+    /**
+     * Test method for SetUpListeners. This test method covers the case where
+     * the all movies button is clicked
+     */
     public void testOnClickSearch()
     {
+        // gets the activity
         mma = this.getActivity();
-        searchButton =
-            (Button)mma.findViewById(R.id.searchButton);
+        // gets the button on the activity
+        searchButton = (Button)mma.findViewById(R.id.searchButton);
+        /**
+         * gets the activity monitor so we can monitor what changes when the
+         * button is clicked
+         */
         ActivityMonitor activityMonitor =
             getInstrumentation().addMonitor(
                 ListMovieActivity.class.getName(),
                 null,
                 false);
         System.out.println(searchButton);
+        // this try-catch block clicks the button necessary as per the test
         try
         {
             runTestOnUiThread(new Runnable() {
@@ -210,14 +279,16 @@ public class MainMovieActivityTest
             System.out.println("Couldnt click button");
             e.printStackTrace();
         }
-
+        // gets the next activity, which would be the list of movies in this
+        // case
         Activity nextActivity =
             getInstrumentation().waitForMonitorWithTimeout(
                 activityMonitor,
                 30000);
+        // asserts that that list exists
         assertNotNull(nextActivity);
+        // finishes that activity
         nextActivity.finish();
     }
-
 
 }
