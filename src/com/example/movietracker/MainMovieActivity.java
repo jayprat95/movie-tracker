c3dc2a0422813e8a128b3dc91310f3a6fad07321
Jayanth Prathipati <jayanth@vt.edu>
2013-12-08 17:05:43 -0500

    commented the MainMovieActivity

--- src/com/example/movietracker/MainMovieActivity.java
+++ src/com/example/movietracker/MainMovieActivity.java
@@ -11,90 +11,169 @@ import android.view.View;
 import android.content.Intent;
 import android.content.res.AssetManager;
 import android.graphics.Color;
-
 import java.util.List;
 import android.os.Bundle;
 import java.util.ArrayList;
 import android.app.Activity;
 
-public class MainMovieActivity extends Activity {
-	// ~Fields............................................................
-	private AssetManager assetManager;
 
-	// ~Methods...........................................................
+// -------------------------------------------------------------------------
+/**
+ * This function represents the Main Splash Screen that is seen when a user
+ * first executes the application. It sets up 4 buttons which link out to the
+ * ListMovieActivity.
+ *
+ * @author jayanth
+ * @version Dec 8, 2013
+ */
+public class MainMovieActivity
+extends Activity
+{
+    // ~Fields............................................................
+    /**
+     * this class lets us get files from the assets folder, such as the JSON
+     * files that we are using in order to get data about Movies for users
+     */
+    private AssetManager assetManager;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		assetManager = this.getAssets();
-		setContentView(R.layout.activity_main_movie_view);
-		this.setActivityBackgroundColor(Color.BLUE);
 
-		EditText text = (EditText) findViewById(R.id.searchText);
+    // ~Methods...........................................................
 
-		text.setFocusable(false);
-		text.setVisibility(View.GONE);
-		// Watch buttons for opening up different views.
-		setupListeners();
+    /**
+     * This function is called as soon as this activity starts up.
+     *
+     * @param savedInstanceState
+     *            is the instance of the application before this activity starts
+     *            up.
+     */
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        /**
+         * gets the asset manager which allows us to access JSON movie files
+         * from the assets folder
+         */
+        assetManager = this.getAssets();
+        // get the view
+        setContentView(R.layout.activity_main_movie_view);
+        this.setActivityBackgroundColor(Color.BLUE);
 
-	}
+        /**
+         * search bar is not currently working, we are disabling it for a later
+         * release.
+         */
+        EditText text = (EditText)findViewById(R.id.searchText);
+        /**
+         * hides the edit text and sets the it so that the edit text can't enter
+         * text
+         */
+        text.setFocusable(false);
+        text.setVisibility(View.GONE);
+        // Watch buttons for opening up different views.
+        setupListeners();
 
-	// ----------------------------------------------------------
-	/**
-	 * This method creates the listeners for the four buttons. They each start a
-	 * new activity via creating an intent.
-	 */
-	public void setupListeners() {
+    }
 
-		Button watched = (Button) findViewById(R.id.watched);
-		watched.setOnClickListener(new Button.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				Intent intent = new Intent(getApplicationContext(),
-						ListMovieActivity.class);
-				intent.putExtra("listname", "watched");
-				startActivity(intent);
-			}
-		});
 
-		Button toWatch = (Button) findViewById(R.id.toWatch);
-		toWatch.setOnClickListener(new Button.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				Intent intent = new Intent(getApplicationContext(),
-						ListMovieActivity.class);
-				intent.putExtra("listname", "toWatch");
-				startActivity(intent);
-			}
-		});
+    // ----------------------------------------------------------
+    /**
+     * This method creates the listeners for the four buttons. They each start a
+     * new activity via creating an intent.
+     */
+    public void setupListeners()
+    {
 
-		Button favorite = (Button) findViewById(R.id.favorite);
-		favorite.setOnClickListener(new Button.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				Intent intent = new Intent(getApplicationContext(),
-						ListMovieActivity.class);
-				intent.putExtra("listname", "favorite");
-				startActivity(intent);
-			}
-		});
+        // gets the button from the view
+        Button watched = (Button)findViewById(R.id.watched);
+        // creates a listener that listens for a touch on the button
+        watched.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            // function that will execute when watched button is clicked
+            public void onClick(View v)
+            {
+                // executes the ListMovieActivity when this button is clicked
+                Intent intent =
+                    new Intent(getApplicationContext(),
+                        ListMovieActivity.class);
+                // tells the ListMovieActivity to use the "watched" list
+                intent.putExtra("listname", "watched");
+                // starts the ListMovieActivity with the intent that was created
+                startActivity(intent);
+            }
+        });
 
-		Button searchButton = (Button) findViewById(R.id.searchButton);
-		searchButton.setOnClickListener(new Button.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				Intent intent = new Intent(getApplicationContext(),
-						ListMovieActivity.class);
-				intent.putExtra("listname", "searchButton");
-				startActivity(intent);
-			}
-		});
+        // gets the button from the view
+        Button toWatch = (Button)findViewById(R.id.toWatch);
+        // creates a listener that listens for a touch on the button
+        toWatch.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            // function that will execute when watched button is clicked
+            public void onClick(View v)
+            {
+                // executes the ListMovieActivity when this button is clicked
+                Intent intent =
+                    new Intent(getApplicationContext(),
+                        ListMovieActivity.class);
+                // tells the ListMovieActivity to use the "toWatch" list
+                intent.putExtra("listname", "toWatch");
+                // starts the ListMovieActivity with the intent that was created
+                startActivity(intent);
+            }
+        });
+        // gets the button from the view
+        Button favorite = (Button)findViewById(R.id.favorite);
+        // creates a listener that listens for a touch on the button
+        favorite.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            // function that will execute when watched button is clicked
+            public void onClick(View v)
+            {
+                // executes the ListMovieActivity when this button is clicked
+                Intent intent =
+                    new Intent(getApplicationContext(),
+                        ListMovieActivity.class);
+                // tells the ListMovieActivity to use the "favorite" list
+                intent.putExtra("listname", "favorite");
+                // starts the ListMovieActivity with the intent that was created
+                startActivity(intent);
+            }
+        });
+        // gets the button from the view
+        Button searchButton = (Button)findViewById(R.id.searchButton);
+        // creates a listener that listens for a touch on the button
+        searchButton.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            // function that will execute when watched button is clicked
+            public void onClick(View v)
+            {
+                // executes the ListMovieActivity when this button is clicked
+                Intent intent =
+                    new Intent(getApplicationContext(),
+                        ListMovieActivity.class);
+                // tells the ListMovieActivity to use the "searchButton" list
+                intent.putExtra("listname", "searchButton");
+                // starts the ListMovieActivity with the intent that was created
+                startActivity(intent);
+            }
+        });
 
-	}
+    }
 
-	public void setActivityBackgroundColor(int color) {
-		View view = this.getWindow().getDecorView();
-		view.setBackgroundColor(color);
-	}
+
+    // ----------------------------------------------------------
+    /**
+     * Sets the background color of an activity
+     *
+     * @param color
+     *            is the color that will be set.
+     */
+    public void setActivityBackgroundColor(int color)
+    {
+        // gets the background window of this application
+        View view = this.getWindow().getDecorView();
+        // sets the color of that background
+        view.setBackgroundColor(color);
+    }
+>>>>>>> Stashed changes
 
 }
--- src/com/example/movietracker/MainMovieActivityTest.java
+++ src/com/example/movietracker/MainMovieActivityTest.java
@@ -12,10 +12,10 @@ import junit.framework.TestCase;
 
 // -------------------------------------------------------------------------
 /**
- * Write a one-sentence summary of your class here. Follow it with additional
- * details about its purpose, what abstraction it represents, and how to use it.
+ * This class tests the Main Movie Activity in order to ensure that it is
+ * working properly
  *
- * @author jayanthprathipati
+ * @author jayanth
  * @version Dec 4, 2013
  */
 
